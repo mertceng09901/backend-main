10f261a328b77e7a0305ee32296f77da43b91f08
Mert Acar <mertacarr56@gamil.com>
2025-12-13 10:48:41 +0300

    değişiklik

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var app = express();
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
