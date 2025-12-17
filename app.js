308515978aaf9889ebd1e785d0598bf71f77e925
Mert Acar <mertacarr56@gamil.com>
2025-12-17 10:37:56 +0300

    çalışılır hale geldi app.js düzeltildi

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
-};
-app.use(allowCrossDomain);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 require('./app_api/models/db');
 var apiRoute= require('./app_api/routes/index');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
